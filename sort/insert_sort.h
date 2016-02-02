385612237ee3b2211a443370842b941d317370a7
zhou78yang <zhou78yang@gmail.com>
2016-02-02 08:39:33 +0800

    Update insert_sort.h

--- sort/insert_sort.h
+++ sort/insert_sort.h
@@ -9,10 +9,12 @@ void InsertSort(T *A, int lo, int hi)
     for(int i = lo+1; i < hi; i++){
         int pos = i; // 使用pos来存储当前需要插入元素的位置
         for(int j = i-1; j >= 0; --j){
+            // 将元素的插入操作与比较一起进行
             if(A[pos] < A[j]){
                 std::swap(A[pos], A[j]);
                 pos = j;
             }
+            else break;
         }
     }
 }
